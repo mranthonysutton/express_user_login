a90192288f6b6b94782b929c50d05c0abc4d50f2
Anthony Sutton <anthony.sutton95@gmail.com>
2020-08-13 15:30:43 -0700

    feat(token): User login now generates a JWT token and passwords are hashed using bcryptjs

--- middleware/users.js
+++ middleware/users.js
@@ -15,4 +15,20 @@ const userAlreadyExists = async (req, res, next) => {
   }
 };
 
-module.exports = { userAlreadyExists };
+const foundUser = async (req, res, next) => {
+  try {
+    const foundUser = await db("users")
+      .where({ email: req.body.email })
+      .first();
+
+    if (!foundUser)
+      return res.status(401).json({ message: "User does not exist" });
+
+    req.foundUser = foundUser;
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { userAlreadyExists, foundUser };
--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const router = express.Router();
 const Users = require("../models/user-models");
 const { validateUserCreation } = require("../middleware/schemaValidation");
-const { userAlreadyExists } = require("../middleware/users");
+const { userAlreadyExists, foundUser } = require("../middleware/users");
 
 router.get("/", async (req, res, next) => {
   try {
@@ -13,12 +15,15 @@ router.get("/", async (req, res, next) => {
 });
 
 router.post(
-  "/",
+  "/register",
   validateUserCreation,
   userAlreadyExists,
   async (req, res, next) => {
     try {
-      const newUser = await Users.add(req.validUser);
+      const newUser = await Users.add({
+        ...req.validUser,
+        password: bcrypt.hashSync(req.validUser.password, 12),
+      });
 
       res.status(201).json(newUser);
     } catch (error) {
@@ -26,5 +31,38 @@ router.post(
     }
   }
 );
+
+router.post("/login", foundUser, async (req, res, next) => {
+  try {
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      req.foundUser.password
+    );
+
+    if (!validPassword)
+      return res.status(404).json({ message: "Invalid credentials" });
+
+    const token = await signToken(req.foundUser);
+
+    res.status(200).json({ token });
+  } catch (error) {
+    next(error);
+  }
+});
+
+function signToken(user) {
+  const payload = {
+    user_id: user.id,
+    email: user.email,
+  };
+
+  const secret = process.env.JWT_SECRET || "secretkey";
+
+  const options = {
+    expiresIn: "1d",
+  };
+
+  return jwt.sign(payload, secret, options);
+}
 
 module.exports = router;
