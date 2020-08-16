6b46087e1cc52c138221cbca03f5d9ed74269107
Anthony Sutton <anthony.sutton95@gmail.com>
2020-08-16 08:10:24 -0700

    feat(user-tests): Changed when db seeds are ran to increase testing speed :sparkles:

--- __tests__/users.js
+++ __tests__/users.js
@@ -7,58 +7,112 @@ afterAll(async () => {
   await db.destroy();
 });
 
-beforeEach(async () => {
-  await db.seed.run();
-});
-
 describe('User Integration Tests', () => {
-  it('GET /api/users', async () => {
-    const response = await request(server).get('/api/users');
-    expect(response.statusCode).toBe(200);
-    expect(response.type).toBe('application/json');
-    expect(response.body.length).toBe(1);
+  let response = {};
+  describe('GET /api/users', () => {
+    beforeAll(async () => {
+      await db.seed.run();
+      response = await request(server).get('/api/users');
+    });
+
+    it('Has status code 200', async () => {
+      expect(response.statusCode).toBe(200);
+    });
+
+    it('Is a JSON response', async () => {
+      expect(response.type).toBe('application/json');
+    });
+
+    it('Has length of 1 user', async () => {
+      expect(response.body.length).toBe(1);
+    });
   });
 
-  it('POST /api/users/register', async () => {
-    const fakeUser = await {
-      email: faker.internet.email(),
-      password: faker.internet.password(),
-    };
-    const response = await request(server)
-      .post('/api/users/register')
-      .send(fakeUser);
-    expect(response.statusCode).toBe(201);
-    expect(response.body.id).toBeGreaterThan(0);
-    expect(response.body.email).toBe(fakeUser.email);
-    expect(response.body.password).toBeDefined();
+  describe('POST /api/users/register (valid)', () => {
+    let fakeUser = {};
+    beforeAll(async () => {
+      fakeUser = await {
+        email: faker.internet.email(),
+        password: faker.internet.password(),
+      };
+
+      response = await request(server)
+        .post('/api/users/register')
+        .send(fakeUser);
+    });
+
+    it('Should have a status code of 201', async () => {
+      expect(response.statusCode).toBe(201);
+    });
+
+    it('Should return a user object', async () => {
+      expect(response.body.id).toBeGreaterThan(0);
+      expect(response.body.email).toBe(fakeUser.email);
+      expect(response.body.password).toBeDefined();
+    });
+
+    it('Should be a JSON object', async () => {
+      expect(response.type).toBe('application/json');
+    });
   });
 
-  it('POST /api/users/login (valid)', async () => {
-    const response = await request(server)
-      .post('/api/users/login')
-      .send({ email: 'test@test.com', password: 'password123' });
+  describe('POST /api/users/login (valid)', () => {
+    beforeAll(async () => {
+      response = await request(server)
+        .post('/api/users/login')
+        .send({ email: 'test@test.com', password: 'password123' });
+    });
 
-    expect(response.statusCode).toBe(200);
-    expect(response.body.token).toBeDefined();
+    it('Should have a status code of 200', async () => {
+      expect(response.statusCode).toBe(200);
+    });
+
+    it('Should return a JWT token', async () => {
+      expect(response.body.token).toBeDefined();
+    });
+
+    it('Should be a JSON object', async () => {
+      expect(response.type).toBe('application/json');
+    });
   });
 
-  it('POST /api/users/login (invalid user)', async () => {
-    const response = await request(server)
-      .post('/api/users/login')
-      .send({ email: 'invalid_user@test.com', password: 'password123' });
+  describe('POST /api/users/login (invalid user)', () => {
+    beforeAll(async () => {
+      response = await request(server)
+        .post('/api/users/login')
+        .send({ email: 'invalid_user@test.com', password: 'password123' });
+    });
 
-    expect(response.statusCode).toBe(404);
-    expect(response.body).toEqual({ message: 'User does not exist' });
-    expect(response.type).toBe('application/json');
+    it('Should return a 404 status code', async () => {
+      expect(response.statusCode).toBe(404);
+    });
+
+    it('Should be a JSON object', async () => {
+      expect(response.type).toBe('application/json');
+    });
+
+    it('Should return proper message', async () => {
+      expect(response.body).toEqual({ message: 'User does not exist' });
+    });
   });
 
-  it('POST /api/users/login (invalid credentials)', async () => {
-    const response = await request(server)
-      .post('/api/users/login')
-      .send({ email: 'test@test.com', password: 'invalid_password' });
+  describe('POST /api/users/login (invalid credentials)', () => {
+    beforeAll(async () => {
+      response = await request(server)
+        .post('/api/users/login')
+        .send({ email: 'test@test.com', password: 'invalid_password' });
+    });
 
-    expect(response.statusCode).toBe(401);
-    expect(response.body).toEqual({ message: 'Invalid credentials' });
-    expect(response.type).toBe('application/json');
+    it('Should return a 401 status code', async () => {
+      expect(response.statusCode).toBe(401);
+    });
+
+    it('Should be a JSON object', async () => {
+      expect(response.type).toBe('application/json');
+    });
+
+    it('Should return proper message', async () => {
+      expect(response.body).toEqual({ message: 'Invalid credentials' });
+    });
   });
 });
